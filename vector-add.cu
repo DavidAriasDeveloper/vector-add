e2cc1326150473e703413505d619a4ad27b4def4
David Arias <davidariasdeveloper@gmail.com>
2017-09-13 15:08:58 -0500

    Shell script fix

--- vector-add.cu
+++ vector-add.cu
@@ -5,7 +5,7 @@
 __global__ void vectorAdd(double *a, double *b, double *c, int n){
   int id = blockIdx.x*blockDim.x+threadIdx.x;
   if(id < n){
-    c[id] = a[id] + b[id]
+    c[id] = a[id] + b[id];
   }
 }
 
